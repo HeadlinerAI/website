c297183615d310aa316f883caa1b3edcce92b3ad
shrayus-masanam <45981228+shrayus-masanam@users.noreply.github.com>
2024-09-14 15:46:22 -0400

    Add theme provider & drawer class

--- src/app/page.js
+++ src/app/page.js
@@ -11,11 +11,29 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import {
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerDescription,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from "@/components/ui/drawer"
+import {
+	Button
+} from "@/components/ui/button"
+import {
+	ThemeProvider
+} from "@/components/theme-provider"
+
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+	  <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>
           <PushNotificationManager/>
           <InstallPrompt/>
@@ -27,7 +45,31 @@ export default function Home() {
     			<CardDescription className="article-summary">Lorem ipsum odor amet, consectetuer adipiscing elit. Bibendum bibendum natoque primis id vehicula libero nulla.</CardDescription>
   			</CardHeader>
 		</Card>
+
+		<Drawer>
+  			<DrawerTrigger>
+				<Card className="news-card">
+      				<CardHeader>
+    					<CardTitle className="article-title">Lorem ipsum odor amet, consectetuer adipiscing elit.</CardTitle>
+    					<CardDescription className="article-summary">Lorem ipsum odor amet, consectetuer adipiscing elit. Bibendum bibendum natoque primis id vehicula libero nulla.</CardDescription>
+  					</CardHeader>
+				</Card>
+			</DrawerTrigger>
+  			<DrawerContent>
+    			<DrawerHeader>
+      				<DrawerTitle>Are you absolutely sure?</DrawerTitle>
+      				<DrawerDescription>This action cannot be undone.</DrawerDescription>
+    			</DrawerHeader>
+    			<DrawerFooter>
+      				<Button>Submit</Button>
+      				<DrawerClose>
+        				<Button variant="outline">Cancel</Button>
+      				</DrawerClose>
+    			</DrawerFooter>
+  			</DrawerContent>
+		</Drawer>
       </main>
+	</ThemeProvider>
     </div>
   );
 }

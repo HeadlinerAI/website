1c72878424fd3057d6c6d2508c33bae4c073e180
Jviguy <jviguytwo2@gmail.com>
2024-09-15 03:06:11 -0400

    export authData in different file

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,37 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { db } from "@/lib/db";
-
-const authOptions = {
-    // Configure one or more authentication providers
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        }),
-        // ...add more providers here
-    ],
-    callbacks: {
-        async signIn({ user, account, profile, email, credentials }) {
-            if (account.provider === 'google') {
-                let udb = await db.users.findFirst({
-                    where: {
-                        email: email
-                    }
-                });
-                if (!udb) {
-                    udb = await db.users.create({
-                        data: {
-                            name: profile.name,
-                            email: email,
-                        }
-                    });
-                }
-                user.userId = udb.id;
-            }
-        }
-    }
-}
+import {authOptions} from "./authData";
 
 const handler = NextAuth(authOptions);
 

1e0691e8cf47d4a29a5f1056fe1abb2d3c104683
JamOsm <125512478+Pandalingg@users.noreply.github.com>
2024-09-15 04:46:07 -0400

    fixed navbar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -35,7 +35,7 @@ import {signIn} from "next-auth/react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 		<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-screen">
 			<div className="hidden">
 				<PushNotificationManager/>
